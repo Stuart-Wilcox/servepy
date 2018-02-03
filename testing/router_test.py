c002d4d31d366662f0babeace55dda22800335f2
Stuart-Wilcox <stuart_wilcox@outlook.com>
2018-02-03 15:56:10 -0500

    Completed testing for Router

--- testing/router_test.py
+++ testing/router_test.py
@@ -56,22 +56,108 @@ class TestRouter(unittest.TestCase):
         self.assertEqual(router._endware_path_match('/abc/123', 'GET')[2], endware_1)
         self.assertEqual(router._endware_path_match('/abc/123', 'GET')[0].params['val'], '123')
         self.assertEqual(router._endware_path_match('/abc/123?val=123', 'GET')[0].query['val'], '123')
-        # print(router._endware_path_match('/abc/123?val=123', 'GET')[0].query)
-
 
     def test_post(self):
-        pass
+        router = Router()
+
+        router.post('/', endware_1)
+        router.post('/abc', endware_2)
+        router.post('/abc/:val', endware_1)
+
+        self.assertIsNotNone(router._endware_path_match('/', 'POST'))
+        self.assertIsNone(router._endware_path_match('/', 'PUT'))
+        self.assertIsNone(router._endware_path_match('/', 'GET'))
+        self.assertIsNone(router._endware_path_match('/', 'DELETE'))
+
+        self.assertIsNotNone(router._endware_path_match('/abc', 'POST'))
+        self.assertIsNotNone(router._endware_path_match('/abc/123', 'POST'))
+
+        self.assertEqual(router._endware_path_match('/', 'POST')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc', 'POST')[2], endware_2)
+        self.assertEqual(router._endware_path_match('/abc/123', 'POST')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc/123', 'POST')[0].params['val'], '123')
+        self.assertEqual(router._endware_path_match('/abc/123?val=123', 'POST')[0].query['val'], '123')
 
     def test_put(self):
-        pass
+        router = Router()
+
+        router.put('/', endware_1)
+        router.put('/abc', endware_2)
+        router.put('/abc/:val', endware_1)
+
+        self.assertIsNotNone(router._endware_path_match('/', 'PUT'))
+        self.assertIsNone(router._endware_path_match('/', 'POST'))
+        self.assertIsNone(router._endware_path_match('/', 'GET'))
+        self.assertIsNone(router._endware_path_match('/', 'DELETE'))
+
+        self.assertIsNotNone(router._endware_path_match('/abc', 'PUT'))
+        self.assertIsNotNone(router._endware_path_match('/abc/123', 'PUT'))
+
+        self.assertEqual(router._endware_path_match('/', 'PUT')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc', 'PUT')[2], endware_2)
+        self.assertEqual(router._endware_path_match('/abc/123', 'PUT')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc/123', 'PUT')[0].params['val'], '123')
+        self.assertEqual(router._endware_path_match('/abc/123?val=123', 'PUT')[0].query['val'], '123')
 
     def test_delete(self):
-        pass
+        router = Router()
+
+        router.delete('/', endware_1)
+        router.delete('/abc', endware_2)
+        router.delete('/abc/:val', endware_1)
+
+        self.assertIsNotNone(router._endware_path_match('/', 'DELETE'))
+        self.assertIsNone(router._endware_path_match('/', 'POST'))
+        self.assertIsNone(router._endware_path_match('/', 'GET'))
+        self.assertIsNone(router._endware_path_match('/', 'PUT'))
+
+        self.assertIsNotNone(router._endware_path_match('/abc', 'DELETE'))
+        self.assertIsNotNone(router._endware_path_match('/abc/123', 'DELETE'))
+
+        self.assertEqual(router._endware_path_match('/', 'DELETE')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc', 'DELETE')[2], endware_2)
+        self.assertEqual(router._endware_path_match('/abc/123', 'DELETE')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc/123', 'DELETE')[0].params['val'], '123')
+        self.assertEqual(router._endware_path_match('/abc/123?val=123', 'DELETE')[0].query['val'], '123')
 
     def test_route(self):
-        pass
+        router = Router()
+
+        router.route('/abc') \
+        .get('/', endware_1) \
+        .get('/:val', endware_2)
+
+        self.assertIsNotNone(router._endware_path_match('/abc', 'GET'))
+        self.assertIsNone(router._endware_path_match('/abc', 'PUT'))
+        self.assertIsNone(router._endware_path_match('/abc', 'POST'))
+        self.assertIsNone(router._endware_path_match('/abc', 'DELETE'))
+
+        self.assertIsNotNone(router._endware_path_match('/abc/123', 'GET'))
+
+        self.assertEqual(router._endware_path_match('/abc', 'GET')[2], endware_1)
+        self.assertEqual(router._endware_path_match('/abc/123', 'GET')[2], endware_2)
+        self.assertEqual(router._endware_path_match('/abc/123', 'GET')[0].params['val'], '123')
+        self.assertEqual(router._endware_path_match('/abc/123?val=123', 'GET')[0].query['val'], '123')
 
     def test_use(self):
-        pass
+        router = Router()
+
+        router.use('/abc', middleware_1)
+        router.use('/abc/123', middleware_2)
+
+        self.assertIsNotNone(router._middleware_path_match('/abc'))
+        self.assertIsNotNone(router._middleware_path_match('/abc/123'))
+        self.assertIsNotNone(router._middleware_path_match('/abc/123/def'))
+
+        self.assertEqual(len(router._middleware_path_match('/abc')), 1)
+        self.assertEqual(len(router._middleware_path_match('/abc/123')), 2)
+        self.assertEqual(len(router._middleware_path_match('/abc/123/def')), 2)
+
+        self.assertEqual(router._middleware_path_match('/abc')[0][1], middleware_1)
+        self.assertEqual(router._middleware_path_match('/abc/123')[0][1], middleware_1)
+        self.assertEqual(router._middleware_path_match('/abc/123')[1][1], middleware_2)
+
+        self.assertEqual(router._middleware_path_match('/abc?val=123')[0][0].query['val'], '123')
+
 
 unittest.main()
